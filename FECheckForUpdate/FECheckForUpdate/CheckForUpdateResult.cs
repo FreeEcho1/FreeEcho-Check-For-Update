51c3696d676be7f15c0d70c925c42f87167e8f95
FreeEcho <20364419+FreeEcho1@users.noreply.github.com>
2022-06-26 08:18:10 +0900

    Add files via upload

--- FECheckForUpdate/FECheckForUpdate/CheckForUpdateResult.cs
+++ FECheckForUpdate/FECheckForUpdate/CheckForUpdateResult.cs
@@ -1,24 +1,20 @@
-﻿namespace FreeEcho
+﻿namespace FreeEcho.FECheckForUpdate;
+
+/// <summary>
+/// 更新確認の結果
+/// </summary>
+public enum CheckForUpdateResult : int
 {
-    namespace FECheckForUpdate
-    {
-        /// <summary>
-        /// 更新確認の結果
-        /// </summary>
-        public enum CheckForUpdateResult : int
-        {
-            /// <summary>
-            /// 更新確認失敗
-            /// </summary>
-            Fail = 0,
-            /// <summary>
-            /// 最新バージョン
-            /// </summary>
-            LatestVersion,
-            /// <summary>
-            /// 最新バージョンではない
-            /// </summary>
-            NotLatestVersion,
-        }
-    }
+    /// <summary>
+    /// 更新確認失敗
+    /// </summary>
+    Fail = 0,
+    /// <summary>
+    /// 最新バージョン
+    /// </summary>
+    LatestVersion,
+    /// <summary>
+    /// 最新バージョンではない
+    /// </summary>
+    NotLatestVersion,
 }
